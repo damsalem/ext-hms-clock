c20edbbfabe33b0be7c8eadabd193c790ceee2f9
damsalem <drummingdan@gmail.com>
2023-03-15 19:27:13 -0400

    Add Military Time Selector Markup and Fn

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,16 @@
 const timeElement = document.querySelector("#time");
+const militarySelector = document.querySelector("#milTime");
+let militaryTime = false;
 
 // Run when extension is clicked
 document.addEventListener("DOMContentLoaded", function () {
     setInterval(updateTime, 1);
+});
+
+// Handle military time toggle
+militarySelector.addEventListener("click", function (e) {
+    militaryTime = !militaryTime;
+    console.log(militaryTime);
 });
 
 function getTime() {
@@ -13,6 +21,10 @@ function getTime() {
     let hours = now.getHours().toString().padStart(2, "0");
     const minutes = now.getMinutes().toString().padStart(2, "0");
     const seconds = now.getSeconds().toString().padStart(2, "0");
+
+    if (militaryTime === true) {
+        return `${hours}:${minutes}:${seconds}`;
+    }
 
     // check if hours is greater than 12 and subtract 12 if it is
     if (hours > 12) {
@@ -27,7 +39,6 @@ function updateTime() {
     timeElement.innerHTML = time;
 }
 
-// TODO: Build 12-24 hour time toggler
 // TODO: Set time toggler in localState
 // TODO: Build color picker markup and fn
 // TODO: Set default color in localState
