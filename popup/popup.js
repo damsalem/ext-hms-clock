91d269eeb093f2572ad2edf93f77b64855d635f5
damsalem <drummingdan@gmail.com>
2023-03-14 21:40:59 -0400

    Set 12 Hour Time as Default, Update Logos

--- popup/popup.js
+++ popup/popup.js
@@ -7,10 +7,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function getTime() {
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2, "0");
+
+    // Get the hours, minutes, seconds
+    let meridiem = "am";
+    let hours = now.getHours().toString().padStart(2, "0");
     const minutes = now.getMinutes().toString().padStart(2, "0");
     const seconds = now.getSeconds().toString().padStart(2, "0");
-    return `${hours}:${minutes}:${seconds}`;
+
+    // check if hours is greater than 12 and subtract 12 if it is
+    if (hours > 12) {
+        meridiem = "pm";
+        hours -= 12;
+    }
+    return `${hours}:${minutes}:${seconds} ${meridiem}`;
 }
 
 function updateTime() {
@@ -18,5 +27,7 @@ function updateTime() {
     timeElement.innerHTML = time;
 }
 
+// TODO: Build 12-24 hour time toggler
+// TODO: Set time toggler in localState
 // TODO: Build color picker markup and fn
 // TODO: Set default color in localState
