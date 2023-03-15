87823ef151e2d8e3a474b9b524eb6e0f7d2aef6a
damsalem <drummingdan@gmail.com>
2023-03-14 20:52:55 -0400

    Add Time in JS

--- popup/popup.js
+++ popup/popup.js
@@ -3,4 +3,5 @@ const time = document.querySelector("#time");
 // Run when extension is clicked
 document.addEventListener("DOMContentLoaded", function () {
     console.log("opened");
+    time.innerHTML = "12:00:00";
 });
