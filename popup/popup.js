2337cfd0ae9e4c468456d1acead92187c78ac4e0
damsalem <drummingdan@gmail.com>
2023-03-14 21:17:29 -0400

    Set Time Using JS

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,19 @@
-const time = document.querySelector("#time");
+const timeElement = document.querySelector("#time");
 
 // Run when extension is clicked
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("opened");
-    time.innerHTML = "12:00:00";
+    setInterval(updateTime, 1);
 });
+
+function getTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+function updateTime() {
+    const time = getTime();
+    timeElement.innerHTML = time;
+}
